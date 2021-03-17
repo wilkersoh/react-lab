a7bc8c6d66f580644f14c64869f41160351bf023
wilkersoh <plgu@hotmail.com>
2021-03-17 20:47:23 +0800

    Update Medium Zoom Image

---
+++ src/MediumImageZoom.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+import Container from "./components/Container";
+import { SliderImages } from "./images/slider";
+
+export default function MediumImageZoom() {
+  return (
+    <Container>
+      <Typography variant={"h2"} gutterBottom>
+        Default
+      </Typography>
+      <Zoom>
+        <Box component={"img"} src={SliderImages[0]} width={300} />
+      </Zoom>
+
+      <Typography variant={"h2"} gutterBottom>
+        Background and zoomMargin
+      </Typography>
+      <Zoom overlayBgColorEnd={"rgb(0,0,0)"} zoomMargin={20}>
+        <Box component={"img"} src={SliderImages[0]} width={300} />
+      </Zoom>
+
+      <Box>
+        You can zoom anything, so <Box component='code'>picture</Box>,{" "}
+        <Box component='code'>figure</Box>, and even{" "}
+        <Box component='code'>div</Box> elements are all valid:
+      </Box>
+
+      <Typography variant={"h2"} gutterBottom>
+        Picture
+      </Typography>
+      <Zoom>
+        <picture>
+          <source media='(max-width: 800px)' srcSet={SliderImages[1]} />
+          <img alt='that wanaka tree' src={SliderImages[1]} width='500' />
+        </picture>
+      </Zoom>
+
+      <Typography variant={"h2"} gutterBottom>
+        Div
+      </Typography>
+      <Zoom>
+        <div
+          aria-label='A blue circle'
+          style={{
+            width: 300,
+            height: 300,
+            borderRadius: "50%",
+            backgroundColor: "#0099ff",
+          }}
+        />
+      </Zoom>
+    </Container>
+  );
+}
--- src/lib/routes.js
+++ src/lib/routes.js
@@ -2,10 +2,12 @@ import CustomPagination from "../components/CustomPagination";
 import MeterialPagination from "../components/MeterialPagination";
 import CustomCircularProgress from "../CustomCircularProgress";
 import CustomSlider from "../CustomSlider";
+import ImageLoadedTrick from "../ImageLoadedTrick";
+import ImageTransitionZoom from "../ImageTransitionZoom";
+import MediumImageZoom from "../MediumImageZoom";
 import MouseMoveZoom from "../MouseMoveZoom";
 import Slider from "../Slider";
 import TableView from "../TableView";
-import ZoomImage from "../ZoomImage";
 
 export const PATHS = [
   {
@@ -34,19 +36,29 @@ export const PATHS = [
     component: CustomCircularProgress,
   },
   {
-    name: "Zoom Image transition",
-    path: "/zoom-image",
-    component: ZoomImage,
-  },
-  {
-    name: "table structure",
-    path: "/table-view",
-    component: TableView,
+    name: "image loaded trick",
+    path: "/image-loaded-trick",
+    component: ImageLoadedTrick,
   },
   {
     name: "mouse move zoom image",
     path: "/mouse-move-zoom-image",
     component: MouseMoveZoom,
+  },
+  {
+    name: "image transition zoom (incompleted)",
+    path: "/image-transition-zoom",
+    component: ImageTransitionZoom,
+  },
+  {
+    name: "medium image zoom",
+    path: "/medium-image-zoom",
+    component: MediumImageZoom,
+  },
+  {
+    name: "table structure (incompleted)",
+    path: "/table-view",
+    component: TableView,
   },
 ];
 
