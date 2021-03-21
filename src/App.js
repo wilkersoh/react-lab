3f3e73325ad41d0d3f4056175a094a20f305c140
wilkersoh <plgu@hotmail.com>
2021-03-22 00:03:10 +0800

    Update Scroll transiton after page changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useLayoutEffect, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
 
 import AppComponent from "./components/AppComponent";
 import Home from "./Home";
@@ -11,14 +17,32 @@ function App() {
     <Router>
       <AppComponent>
         <Switch>
-          <Route exact path='/' component={Home} />
-          {PATHS.map(({ path, component }) => (
-            <Route key={path} path={path} component={component} />
-          ))}
+          <Routes />
         </Switch>
       </AppComponent>
     </Router>
   );
 }
+
+const Routes = () => {
+  const location = useLocation();
+  // Scroll to top if path changes
+  /**
+      1. useEffect: render components -> paint to screen -> scroll to top (run effect),(need to add css scroll-behavior: smooth; in html tag)
+      2. useLayoutEffect: render components -> scroll to top (run effect) -> paint to screen
+   */
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <>
+      <Route exact path='/' component={Home} />
+      {PATHS.map(({ path, component }) => (
+        <Route key={path} path={path} component={component} />
+      ))}
+    </>
+  );
+};
 
 export default App;
