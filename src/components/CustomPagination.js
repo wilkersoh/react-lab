c31833f377728a699ea626df948039a2b9d16bce
wilkersoh <plgu@hotmail.com>
2021-03-23 16:52:54 +0800

    Update Customise Paginate

--- src/components/CustomPagination.js
+++ src/components/CustomPagination.js
@@ -1,32 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import { Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from './Container';
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "./Container";
 
-const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
 
 const usePaginateStyles = makeStyles((theme) => ({
   ui: {
-    display: 'flex',
-    listStyle: 'none',
-    alignItems: 'center',
-    '& li': {
-      margin: '0 4px',
-      '& a': {
-        display: 'inline-block',
-        padding: '6px',
-        textDecoration: 'none',
-        borderRadius: '4px',
-        '&.active': {
-          background: 'green',
-          color: 'white',
-        },
-        '&:hover': {
-          color: 'black',
-        },
+    display: "flex",
+    listStyle: "none",
+    alignItems: "center",
+    "& li": {
+      padding: "0 4px",
+      background: "#aeaeae",
+      color: "white",
+      cursor: "pointer",
+      "&.active": {
+        background: "#585858",
+      },
+      "&:hover": {
+        color: "black",
       },
     },
   },
@@ -51,9 +47,9 @@ export default function CustomPagination() {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const indexOfLastPost = currentPage * postsPerPage; // 10
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; // 10 - 10 = 0
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost); // (0, 10)
 
   if (loading) return <h2>loading..</h2>;
 
@@ -69,36 +65,82 @@ export default function CustomPagination() {
         postsPerPage={postsPerPage}
         paginate={paginate}
         currentPage={currentPage}
+        paginateLength={5} // 7 or 5; control show paginate number length
       />
     </Container>
   );
 }
 
-const Pagination = ({ totalPosts, postsPerPage, paginate, currentPage }) => {
+const Pagination = ({
+  totalPosts,
+  postsPerPage,
+  paginate,
+  currentPage,
+  paginateLength,
+}) => {
   const classes = usePaginateStyles();
   const pageNumbers = [];
+  const pages = Math.ceil(totalPosts / postsPerPage); //  total Page length
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
+  // currentPage = 5; paginateLength = 5; 5 / 2 = 2; maxLeft = 5 - 2 = 3
+  let maxLeft = currentPage - Math.floor(paginateLength / 2); // if click 5 then show 3 4, that is 2 mean, the nearest 2 number
+  let maxRight = currentPage + Math.floor(paginateLength / 2);
+
+  if (maxLeft < 1) {
+    maxLeft = 1;
+    maxRight = paginateLength;
   }
+
+  if (maxRight > pages) {
+    maxLeft = pages - (paginateLength - 1);
+
+    maxRight = pages;
+    if (maxLeft < 1) maxLeft = 1;
+  }
+
+  for (let page = maxLeft; page <= maxRight; page++) {
+    pageNumbers.push(page);
+  }
+
+  // for (let i = 1; i <= pages; i++) {
+  // 1 2 3 4 5 ... 10 (default) length 7
+  // 1 2 3 4 5 ... 10 (select 4) length 7 前面select 到5 才變化
+  // 1 ... 4 5 6 ... 10 (select 5) length 7
+  // 1 ... 5 6 7 ... 10 (select 6) length 7
+  // 1 ... 15 16 17 ... 20 (select 16) select 到16 才變化
+  // 1 ... 16 17 18 19 20 (select 17)
+  // 1 ... 14 15 16 ... 20 (select 15)
+  // < 1 2 3 4 5 > (default) length 5
+  // < 2 3 4 5 6 >  (select 4) length 5
+  // < 3 4 5 6 7 >  (select 5) length 5
+  // pageNumbers.push(i);
+  // }
+
+  const nextPaginate = () => {
+    const nextPage = currentPage + 1;
+    paginate(nextPage);
+  };
+  const prevPaginate = () => {
+    const prevPage = currentPage - 1;
+    paginate(prevPage);
+  };
 
   return (
     <nav>
       <ul className={classes.ui}>
-        <li>
+        <li
+          style={{ display: currentPage !== 1 ? "block" : "none" }}
+          onClick={prevPaginate}>
           <Box>&laquo;</Box>
         </li>
         {pageNumbers.map((number) => (
-          <li key={number}>
-            <Box
-              onClick={() => paginate(number)}
-              className={currentPage === number ? 'active' : null}
-            >
-              {number}
-            </Box>
+          <li key={number} className={currentPage === number ? "active" : null}>
+            <Box onClick={() => paginate(number)}>{number}</Box>
           </li>
         ))}
-        <li>
+        <li
+          style={{ display: currentPage !== pages ? "block" : "none" }}
+          onClick={nextPaginate}>
           <Box>&raquo;</Box>
         </li>
       </ul>
