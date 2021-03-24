cb77741bc85d3b521d352821bf4fc1d02ddc696d
wilkersoh <plgu@hotmail.com>
2021-03-24 14:27:24 +0800

    Update Slider, use translate3d control

--- src/CustomSlider.js
+++ src/CustomSlider.js
@@ -23,9 +23,8 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   sliderContent: {
-    transform: (props) => `translateX(-${props.transform}px)`,
+    transform: (props) => `translate3d(-${props.transform}%, 0, 0)`, // use 3d instead of translateX
     transition: (props) => `transform ease-out ${props.transition}s`,
-    // width: (props) => `${props.width}px`,
     height: "100%",
     display: "flex",
   },
@@ -38,37 +37,10 @@ export default function CustomSlider() {
     width: 0,
   });
 
-  // const getWidth = () => window.innerWidth;
-  let resizeWindow = () => {
-    setActiveIndex((prev) => {
-      return {
-        transform: prev.transform, // use prev index when dom resize
-        width: containerRef.current.offsetWidth,
-      };
-    });
-  };
-
-  useEffect(() => {
-    // get slide container width
-    const width = containerRef.current.offsetWidth;
-
-    setActiveIndex({ ...activeIndex, width });
-
-    resizeWindow();
-
-    window.addEventListener("resize", resizeWindow);
-
-    return () => window.removeEventListener("resize", resizeWindow);
-  }, []);
-
   const classes = useStyles({
-    // transform: activeIndex.transform * getWidth(),
-    transform: activeIndex.transform * activeIndex.width,
+    transform: activeIndex.transform * 100,
     transition: 0.45,
-    // width: getWidth() * SliderImages.length,
   });
-
-  console.log("activeIndex :>> ", activeIndex);
 
   const nextSlide = () => {
     if (activeIndex.transform === SliderImages.length - 1) return;
@@ -87,7 +59,10 @@ export default function CustomSlider() {
   return (
     <Container d='flex' flex={1} height='400px' width='100%'>
       <Box className={classes.slider}>
-        <Box className={classes.sliderContent} ref={containerRef}>
+        <Box
+          component='ul'
+          className={classes.sliderContent}
+          ref={containerRef}>
           {SliderImages.map((image, i) => (
             <Box
               component={"img"}
