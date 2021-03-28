a4744ddcc6518f02b183bf1ccda3111cbe9bb713
wilkersoh <plgu@hotmail.com>
2021-03-28 21:18:14 +0800

    Update Life Content Up

--- src/MemoAndTrick.js
+++ src/MemoAndTrick.js
@@ -28,6 +28,15 @@ export default function MemoAndTrick() {
         不要用 React.memo也能做到在 Context Provider下 不相關component
         不re-render
       </Typography>
+      <Typography variant='h5'>
+        We split the MemoAndTrick component in two. The parts that depend on the
+        color, together with the color state variable itself, have moved into
+        MyProvider. The parts that don’t care about the color stayed in the
+        MemoAndTrick component and are passed to MyProvider as JSX content, also
+        known as the children prop. When the color changes, MyProvider
+        re-renders. But it still has the same children prop it got from the
+        MemoAndTrick last time, so React doesn’t visit that subtree.
+      </Typography>
       {/*
           和 Memo 一樣效果
           為什麼 它會做到 和Memo一樣呢？ 它明明 也 被change state了
@@ -40,7 +49,6 @@ export default function MemoAndTrick() {
               那為什麼 它的 state 被改變了 DirectChild 不會 re-render呢？
 
         someone: The reason the second fix works: In both cases, App is responsible for rendering DirectChild, so in both cases, DirectChild will re-render when App re-renders.  After the refactoring, App is no longer being re-rendered when the state changes; only MyProvider is. Because App no longer needs to be re-rendered, DirectChild does not need to be re-rendered.
-
         */}
       <MyProvider>
         <DirectChild />
@@ -50,6 +58,7 @@ export default function MemoAndTrick() {
 }
 
 const DirectChild = () => {
+  console.log("direct child re-render");
   return (
     <Box>
       <NestedChild />
